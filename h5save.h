f888bf3161d5fcc708ea4b4756db9c26a2ad5554
Bharath M R <catchmrbharath@gmail.com>
2013-01-30 12:25:55 +0530

    removed files from git status

--- h5save.h
+++
@@ -1,9 +0,0 @@
-#ifndef _H5_SAVE_
-#define _H5_SAVE_
-
-#include "hdf5.h"
-
-int createfile(char * name);
-int create_new_dataset(char * name, int ticks, float * fields, int xdim,
-                        int ydim);
-#endif
