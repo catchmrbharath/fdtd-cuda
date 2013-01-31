993b9b7b5c476f791f4a0f1475cf877c5e3dc3b0
Bharath M R <catchmrbharath@gmail.com>
2013-01-31 15:35:26 +0530

    new constants file

---
+++ src/constants.h
@@ -0,0 +1,9 @@
+#ifndef __CONSTANTS__
+#define __CONSTANTS__
+#define PI 3.1415926535897932f
+#define MAX_VOL 1.0f
+#define MIN_VOL 0.00001f
+#define LIGHTSPEED 299792458
+#define EPSILON 8.8541878176e-12f
+#define MU 1.2566370614e-6f
+#endif
